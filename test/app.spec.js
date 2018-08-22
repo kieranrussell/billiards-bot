b8edfd23f9a4d420fea57f038285f4f5645c4891
Neil <neil.langford@live.com>
2018-08-22 22:46:13 +0100

    Added a test

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,4 +4,8 @@ describe('test', () => {
     it('should return a string', () => {
         expect('ci with travis').to.equal('ci with travis');
     });
+
+    it('should do simple maths', () => {
+        expect(1).to.equal(2-1);
+    });
 });
