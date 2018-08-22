e26c60792793168867784ebb0972576f57eb8bfe
Neil <neil.langford@live.com>
2018-08-23 00:43:55 +0100

    removed mock tests.

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,14 +3,6 @@ const app = require('../src/app');
 const matches = app.matches;
 
 describe('test', () => {
-    it('should return a string', () => {
-        expect('ci with travis').to.equal('ci with travis');
-    });
-
-    it('should do simple maths', () => {
-        expect(1).to.equal(2-1);
-    });
-
     it('should return matches as an array', () => {
         expect(matches).to.be.an('array');
     });
