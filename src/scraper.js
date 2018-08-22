6807aad1c45b16426103ae9945dc51e9f59a3860
Kieran Russell <kieran@russell.scotnet.co.uk>
2018-08-22 19:28:57 +0100

    Scrape to get todays matches.

---
+++ src/scraper.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Kieran on 22/08/2018.
+ */
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+const options = {
+    uri: `http://www.snooker.org/res/index.asp?template=24`,
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
+
+function getGames(){
+    return new Promise(function(resolve, reject){
+        rp(options)
+            .then(($) => {
+                var games = [];
+                $('table#latest > tbody > tr').each((i, item) => {
+                    var game = {
+                        player: $('td.player:nth-child(3) > a', item).text().trim(),
+                        opponent: $('td.player:nth-child(8) > a', item).text().trim(),
+                        time: $('td.scheduled', item).text().trim()
+                    };
+                    games.push(game);
+                });
+
+                return games;
+            }).then((games) => {
+            resolve(games);
+        }).catch((err) => {
+            console.log(err);
+        });
+    })
+}
