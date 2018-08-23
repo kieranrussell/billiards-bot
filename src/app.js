52efb92a7b49c089e91001b11ddb42494947945f
Neil <neil.langford@live.com>
2018-08-23 02:46:02 +0100

    added dev branch

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 
 const matches = require('./matches');
+const reddit = require('./reddit');
+
 const http = require('http');
-//const Snoowrap = require('snoowrap');
-//const Snoostorm = require('snoostorm');
+
+const client = reddit.client;
+
+const streamOpts = {
+    subreddit: process.env.SUBREDDIT_NAME,
+    results: 25
+};
+
+const comments = client.CommentStream(streamOpts);
+
 
 let todaysMatches = [];
 
@@ -11,99 +21,82 @@ matches().then(function(data) {
     todaysMatches = data;
 });
 
+comments.on('comment', function (comment) {
+    console.log(comment);
+    if (comment.body.indexOf('billiardbot') !== -1) {
+        getCommentReply(comment).then(function(reply){
+            comment.reply(reply);
+        }).catch(function(error){
+            console.log(error);
+        });
+    }
+});
 
-// const r = new Snoowrap({
-//     userAgent: 'billiard-bot',
-//     clientId: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     username: process.env.REDDIT_USER,
-//     password: process.env.REDDIT_PASS
-// });
-// const client = new Snoostorm(r);
+function getData(url) {
+    console.log(url);
+    return new Promise(function(resolve, reject){
+        http.get(url, function (resp) {
+            var data = '';
 
-// const streamOpts = {
-//     subreddit: process.env.SUBREDDIT_NAME,
-//     results: 25
-// };
+            resp.on('data', function (chunk) {
+                data += chunk;
+            });
 
-// const comments = client.CommentStream(streamOpts);
+            resp.on('end', function () {
+                resolve(JSON.parse(data));
+            });
 
-// comments.on('comment', function (comment) {
-//     console.log(comment);
-//     if (comment.body.indexOf('billiardbot') !== -1) {
-//         getCommentReply(comment).then(function(reply){
-//             comment.reply(reply);
-//         }).catch(function(error){
-//             console.log(error);
-//         });
-//     }
-// });
+        }).on("error", function (err) {
+            reject(JSON.parse(err));
+        });
+    });
+}
 
-// function getData(url) {
-//     console.log(url);
-//     return new Promise(function(resolve, reject){
-//         http.get(url, function (resp) {
-//             var data = '';
+function getDateString(string){
+    var date = new Date(string);
+    date = date.toString().split(':');
+    return date[0].substring(0, date[0].length - 2);
+}
 
-//             resp.on('data', function (chunk) {
-//                 data += chunk;
-//             });
+function getCommentReply(comment){
+    return new Promise(function(resolve, reject){
+        var keywords = comment.body.split(' ').map(function (keyword) {
+            return keyword.trim();
+        }).filter(function (keyword) {
+            return keyword && keyword !== 'billiardbot';
+        });
+        console.log(keywords);
 
-//             resp.on('end', function () {
-//                 resolve(JSON.parse(data));
-//             });
+        var returnData= {
+            name: '',
+            date: '',
+            eventName: ''
+        };
 
-//         }).on("error", function (err) {
-//             reject(JSON.parse(err));
-//         });
-//     });
-// }
-
-// function getDateString(string){
-//     var date = new Date(string);
-//     date = date.toString().split(':');
-//     return date[0].substring(0, date[0].length - 2);
-// }
-
-// function getCommentReply(comment){
-//     return new Promise(function(resolve, reject){
-//         var keywords = comment.body.split(' ').map(function (keyword) {
-//             return keyword.trim();
-//         }).filter(function (keyword) {
-//             return keyword && keyword !== 'billiardbot';
-//         });
-//         console.log(keywords);
-
-//         var returnData= {
-//             name: '',
-//             date: '',
-//             eventName: ''
-//         };
-
-//         getData('http://api.snookerorg.ardalen.com/?t=10&s=2017').then(function(response){
-//             var playerId = response.filter(function(player){
-//                 return keywords.indexOf(player.FirstName) !== -1 && keywords.indexOf(player.LastName) !== -1
-//             }).map(function(player){
-//                 returnData.name = player.FirstName.trim() + ' '+ player.MiddleName.trim() + ' '+ player.LastName.trim();
-//                 return player.ID;
-//             })[0];
-//             return getData('http://api.snookerorg.ardalen.com/?t=8&p=' + playerId + '&s=2017');
-//         }).then(function(response){
-//             var eventId = response[0].EventID;
-//             returnData.date = response[0].ScheduledDate || response[0].InitDate;
-//             return getData('http://api.snookerorg.ardalen.com/?e=' + eventId);
-//         }).then(function(response){
-//             returnData.eventName = response.Name;
-//         }).then(function(){
-//             var reply = returnData.name.trim() + ' is playing in the ' + returnData.eventName.trim() + ' on ' + getDateString(returnData.date);
-//             console.log(returnData);
-//             console.log(reply);
-//             resolve(reply);
-//         }).catch(function(error){
-//             reject(error);
-//         });
-//     });
-//}
+        getData('http://api.snookerorg.ardalen.com/?t=10&s=2017').then(function(response){
+            var playerId = response.filter(function(player){
+                return keywords.indexOf(player.FirstName) !== -1 && keywords.indexOf(player.LastName) !== -1
+            }).map(function(player){
+                returnData.name = player.FirstName.trim() + ' '+ player.MiddleName.trim() + ' '+ player.LastName.trim();
+                return player.ID;
+            })[0];
+            return getData('http://api.snookerorg.ardalen.com/?t=8&p=' + playerId + '&s=2017');
+        }).then(function(response){
+            var eventId = response[0].EventID;
+            returnData.date = response[0].ScheduledDate || response[0].InitDate;
+            return getData('http://api.snookerorg.ardalen.com/?e=' + eventId);
+        }).then(function(response){
+            returnData.eventName = response.Name;
+        }).then(function(){
+            var reply = returnData.name.trim() + ' is playing in the ' + returnData.eventName.trim() + ' on ' + getDateString(returnData.date);
+            console.log(returnData);
+            console.log(reply);
+            resolve(reply);
+        }).catch(function(error){
+            reject(error);
+        });
+    });
+}
 
 module.exports = {
     matches: todaysMatches
---
+++ src/reddit.js
@@ -0,0 +1,14 @@
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+
+const r = new Snoowrap({
+    userAgent: 'billiard-bot',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+
+module.exports = {
+  client: new Snoostorm(r)
+};
