39d239e5940e312a9fa3f9900bff2108dfd894b7
Kieran Russell <kieran@russell.scotnet.co.uk>
2018-08-28 23:02:49 +0100

    Refactoring of scheduled tasks to make generic config file.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,4 @@
 require('dotenv').config();
-const schedule = require('node-schedule');
-const reddit = require('./reddit');
-const matchesController = require('./matches/matches.controller');
+const scheduler = require('./scheduler/app.scheduler');
 
-schedule.scheduleJob('0 * * * *', () => {
-    console.log('Running daily job to post update to subreddit');
-    postDailyUpdateToSubReddit(matchesController);
-});
-
-function postDailyUpdateToSubReddit(matchesController){
-    //Post daily tournament/matches to subreddit
-    matchesController.getDailyTournamentPost().then((dailyTournamentPost) => {
-        reddit.r.submitSelfpost(dailyTournamentPost).then(console.log).catch(console.error);
-    }).catch((error) => {
-        console.log(error);
-    });
-}
+scheduler.initialise();
