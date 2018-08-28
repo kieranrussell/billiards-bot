641f7d6c694b508234b60177ee41b1352f60e28e
Neil <neil.langford@live.com>
2018-08-28 14:15:58 +0100

    Changes for heroku worker.

--- src/app.js
+++ src/app.js
@@ -2,18 +2,6 @@ require('dotenv').config();
 const schedule = require('node-schedule');
 const reddit = require('./reddit');
 const matchesController = require('./matches/matches.controller');
-const express = require('express');
-const app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
 
 schedule.scheduleJob('* 6 * * *', () => {
     console.log('Running daily job to post update to subreddit');
