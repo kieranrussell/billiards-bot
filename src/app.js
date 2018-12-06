453e14b3047d2825fc2f7ff4a155ce0e459f00d2
Neil Langford <neil.langford@live.com>
2018-12-06 23:27:59 +0000

    getting dev working

--- src/app.js
+++ src/app.js
@@ -1,41 +1,4 @@
 require('dotenv').config();
 const scheduler = require('./scheduler/app.scheduler');
 
-const matchesData = require('./matches/matches.data-access');
-const express = require('express');
-const app = express();
-
-let port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    matchesData.get()
-    .then((data) => {
-        console.log('Returning data');        
-        return res.send(JSON.stringify(data));
-    })
-    .catch((error) => {
-        console.log(error);
-        return res.send(error);
-    });
-});
-
-app.get('/:name', (req, res) => {
-    matchesData.get()
-    .then((data) => {
-        console.log('Returning data');
-        let match = data.matches.filter((item) => {
-            return item.player.includes(req.params.name) || item.opponent.includes(req.params.name);
-        })        
-        return res.send(JSON.stringify(match));
-    })
-    .catch((error) => {
-        console.log(error);
-        return res.send(error);
-    });
-});
-
-app.listen(port, () => {
-    console.log('Server is listening on port ' + port);
-});
-
 scheduler.initialise();
